bd729f0553b3dab94d2ddb5529f64f2f74c87c7e
xuekai1 <xuekai1@jd.com>
2019-06-11 21:07:21 +0800

    net db limitapp

---
+++ dbutils/src/main/java/com/xk/dbutils/DBUtils.java
@@ -0,0 +1,12 @@
+package com.xk.dbutils;
+
+/**
+ * 1.0.0-SNAPSHOT
+ * @author xuekai1
+ * @date 2019-06-11
+ */
+public class DBUtils {
+    public void test(){
+        System.out.println("导入了litepal");
+    }
+}
---
+++ netutils/src/main/java/com/xk/netutils/FrescoUtils.java
@@ -0,0 +1,8 @@
+package com.xk.netutils;
+
+/**
+ * @author xuekai1
+ * @date 2019-06-11
+ */
+public class FrescoUtils {
+}
