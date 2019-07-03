9a0bb04ee4a151f4503b3175c621bf7a4a963e07
xuekai1 <xuekai1@jd.com>
2019-07-03 17:15:39 +0800

    添加异步调用

--- netutils/src/main/java/com/xk/netutils/NetUtil.java
+++ netutils/src/main/java/com/xk/netutils/NetUtil.java
@@ -3,6 +3,7 @@ package com.xk.netutils;
 import com.xk.netutils.rx.RxScheduleHelper;
 
 import io.reactivex.Observable;
+import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -72,12 +73,31 @@ public class NetUtil {
     }
 
     /**
+     * 发起get请求
+     *
+     * @param url_headers_tag String Headers Object
+     */
+    public void get(Callback callback, Object... url_headers_tag) {
+        Headers headers;
+        if (getParams(url_headers_tag, 1) == null) {
+            headers = new Headers.Builder().build();
+        } else {
+            headers = (Headers) getParams(url_headers_tag, 1);
+        }
+        Request request = new Request.Builder()
+                .headers(headers)
+                .url((String) getParams(url_headers_tag, 0))
+                .tag(getParams(url_headers_tag, 2))
+                .build();
+        new Thread(() -> getOkHttpClient().newCall(request).enqueue(callback)).start();
+    }
+
+    /**
      * 发起post请求
      *
      * @param url_headers_formBody String Headers FormBody
-     * @return
      */
-    private Observable<Response> post(Object... url_headers_formBody) {
+    public Observable<Response> post(Object... url_headers_formBody) {
         Headers headers;
         if (getParams(url_headers_formBody, 1) == null) {
             headers = new Headers.Builder().build();
@@ -94,6 +114,26 @@ public class NetUtil {
             Response response = getOkHttpClient().newCall(request).execute();
             emitter.onNext(response);
         }).compose(RxScheduleHelper.io_main());
+    }
+
+    /**
+     * 发起post请求
+     *
+     * @param url_headers_formBody String Headers FormBody
+     */
+    public void post(Callback callback, Object... url_headers_formBody) {
+        Headers headers;
+        if (getParams(url_headers_formBody, 1) == null) {
+            headers = new Headers.Builder().build();
+        } else {
+            headers = (Headers) getParams(url_headers_formBody, 1);
+        }
+        Request request = new Request.Builder()
+                .headers(headers)
+                .method("post", (RequestBody) getParams(url_headers_formBody, 2))
+                .url((String) getParams(url_headers_formBody, 0))
+                .build();
+        new Thread(() -> getOkHttpClient().newCall(request).enqueue(callback)).start();
     }
 
     /**
